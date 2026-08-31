127e2594b2585126156789bb2b793516972b28af
mdkazinaim <kazinaim121@gmail.com>
2026-08-31 08:18:46 +0600

    refactor: remove build artifacts and update webpack configurations

--- dist/auth.controller.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Response } from 'express';
-export declare class AuthController {
-    githubAuth(res: Response): Response<any, Record<string, any>> | undefined;
-    githubAuthCallback(code: string, res: Response): Promise<void | Response<any, Record<string, any>>>;
-}
--- dist/root.controller.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class RootController {
-    getRoot(): {
-        status: string;
-        message: string;
-        timestamp: string;
-    };
-}
